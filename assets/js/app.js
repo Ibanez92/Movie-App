acf66300728f18ece1163aeb3735b5e4c2f9f340
Javier Ibanez <expertprep2015@gmail.com>
2021-03-03 02:20:51 -0500

    added trailers to my page when the movie image is clicked and fetched

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,11 @@ const url = 'https://api.themoviedb.org/3/search/movie?api_key=0f46154c6e7f81344
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
 const movieSearchable = document.querySelector('#movies-searchable');
-const imgElement = document.querySelector('img');
 
+function generateUrl(path) {
+    const url = `https://api.themoviedb.org/3${path}?api_key=0f46154c6e7f8134478e0cd0f9b48dc6`;
+    return url;
+}
 
 function movieSection(movies) {
      return movies.map((movie) => {
@@ -53,8 +56,8 @@ function renderSearchMovies(data) {
 buttonElement.onclick = function(event) {
     event.preventDefault();
     const value = inputElement.value;
-
-    const newUrl = url + '&query=' + value;
+    const path = '/search/movie';
+    const newUrl = generateUrl(path) + '&query=' + value;
     
     fetch(newUrl)
         .then((res) => res.json())
@@ -67,16 +70,52 @@ buttonElement.onclick = function(event) {
     console.log('Value ', value);
 }
 
+function createIframe(video) {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${video.key}`;
+    iframe.width = 360;
+    iframe.height = 315;
+    iframe.allowFullscreen = true;
+
+    return iframe;
+}
+
 // Event delegation
 document.onclick = function(event) {
    
     const target = event.target;
 
     if (target.tagName.toLowerCase() === 'img') {
-        console.log('hello world');
+        const movieId = target.dataset.movieId;
+        console.log('Movie ID: ', movieId);
         const section = event.target.parentElement; //section
         const content = section.nextElementSibling; //content
         content.classList.add('content-display');
+
+        const path = `/movie/${movieId}/videos`;
+        const url = generateUrl(path);
+        //fetch movie videos
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                // TODO
+                // display movie videos
+                console.log('Videos: ', data);
+                const videos = data.results;
+                const length = videos.length > 4 ? 4 : videos.length;
+                const iframeContainer = document.createElement('div');
+
+                for (let i = 0; i < length; i++) {
+                    const video = videos[i]; // video
+                    const iframe = createIframe(video);
+                    iframeContainer.appendChild(iframe);
+                    content.appendChild(iframeContainer);
+                }
+            })
+            .catch((error) => {
+                console.log('Error: ', error);
+            });
+
     }
 
     if (target.id === 'content-close') {
