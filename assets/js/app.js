2e5b1d5bf10a09ec98a5208de08577915e3d3de2
Javier Ibanez <expertprep2015@gmail.com>
2021-03-04 23:58:50 -0500

    added apiTransaction.js file to handle all of the api requests

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,8 @@
-// Initial Values
-const API_KEY = "0f46154c6e7f8134478e0cd0f9b48dc6";
-const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-
-const url =
-  "https://api.themoviedb.org/3/search/movie?api_key=0f46154c6e7f8134478e0cd0f9b48dc6";
-
 // Selecting elements from the DOM
 const buttonElement = document.querySelector("#search");
 const inputElement = document.querySelector("#inputValue");
 const movieSearchable = document.querySelector("#movies-searchable");
-
-function generateUrl(path) {
-  const url = `https://api.themoviedb.org/3${path}?api_key=0f46154c6e7f8134478e0cd0f9b48dc6`;
-  return url;
-}
+const moviesContainer = document.querySelector('#movie-container')
 
 function movieSection(movies) {
   return movies.map((movie) => {
@@ -45,25 +34,28 @@ function createMovieContainer(movies) {
 
 function renderSearchMovies(data) {
   // data.results []
-  movieSearchable.innerHTML = "";
+  movieSearchable.innerHTML = '';
   const movies = data.results;
   const movieBlock = createMovieContainer(movies);
   movieSearchable.appendChild(movieBlock);
-  console.log("Data: ", data);
+}
+
+function renderMovies(data) {
+    // data.results []
+    const movies = data.results;
+    const movieBlock = createMovieContainer(movies);
+    moviesContainer.appendChild(movieBlock);
+    
+}
+
+function handleError(error) {
+    console.log('Error ', error);
 }
 
 buttonElement.onclick = function (event) {
   event.preventDefault();
   const value = inputElement.value;
-  const path = "/search/movie";
-  const newUrl = generateUrl(path) + "&query=" + value;
-
-  fetch(newUrl)
-    .then((res) => res.json())
-    .then(renderSearchMovies)
-    .catch((error) => {
-      console.log("Error: ", error);
-    });
+  searchMovie(value);
 
   inputElement.value = "";
   console.log("Value ", value);
@@ -123,3 +115,9 @@ document.onclick = function (event) {
     content.classList.remove("content-display");
   }
 };
+
+getUpcomingMovies();
+
+getTopRatedMovies();
+
+getPopularMovies();
